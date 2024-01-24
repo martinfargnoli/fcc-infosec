2bbf7cb776ac5b16a2b4fdd32544bfb479fe3aa1
Martín Fargnoli <mfargnoli@outlook.com>
2024-01-23 23:04:51 -0300

    Infosec con HelmetJS

---
+++ boilerplate-bcrypt/server.js
@@ -0,0 +1,59 @@
+'use strict';
+const express     = require('express');
+const bodyParser  = require('body-parser');
+const fccTesting  = require('./freeCodeCamp/fcctesting.js');
+const app         = express();
+const helmet      = require('helmet');
+const bcrypt      = require('bcrypt');
+fccTesting(app);
+const saltRounds = 12;
+const myPlaintextPassword = 'sUperpassw0rd!';
+const someOtherPlaintextPassword = 'pass123';
+
+let ninetyDaysInSeconds = 90 * 24 * 60 * 60;
+
+app.use(helmet.hidePoweredBy());
+app.use(helmet.xssFilter());
+app.use(helmet.noSniff());
+app.use(helmet.ieNoOpen());
+app.use(helmet.hsts({ maxAge: ninetyDaysInSeconds, force: true }));
+app.use(helmet.noCache());
+app.use(
+  helmet({
+    frameguard: {
+      action: "deny",
+    },
+    contentSecurityPolicy: {
+      directives: {
+        defaultSrc: ["'self'"],
+        scriptSrc: ["'self'", "trusted-cdn.com"],
+      },
+    },
+    dnsPrefetchControl: false,
+  }),
+);
+
+helmet.hidePoweredBy({ setTo: "Hidden" });
+
+//START_ASYNC -do not remove notes, place code between correct pair of notes.
+
+bcrypt.hash(myPlaintextPassword, saltRounds, (err, hash) => {
+  console.log(hash);
+  //$2a$12$Y.PHPE15wR25qrrtgGkiYe2sXo98cjuMCG1YwSI5rJW1DSJp0gEYS
+  bcrypt.compare(myPlaintextPassword, hash, (err, res) => {
+    console.log(res); //true
+  });
+});
+
+//END_ASYNC
+
+//START_SYNC
+
+var hash = bcrypt.hashSync(myPlaintextPassword, saltRounds);
+console.log(hash);
+var result = bcrypt.compareSync(myPlaintextPassword, hash);
+console.log(result);
+
+//END_SYNC
+
+app.listen(process.env.PORT || 3000, () => {});
